fd353c3e5c8fab12d2dd53ce56bf116354e727dc
Stefany Laforest <slaforest@spiria.com>
2022-04-19 12:41:32 -0400

    checkbox input with css

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -2,20 +2,26 @@ import React, { useState } from "react";
 import TextInput from "../ui-library/textInput/textInput";
 import NumberInput from "../ui-library/numberInput/numberInput";
 import Select from "../ui-library/select/select";
+import CheckboxInput from "../ui-library/checkboxInput/checkboxInput";
 
 const Demo: React.FunctionComponent = () => {
   const [formValues, setFormValues] = useState({
     name: "",
     age: "",
     country: "",
+    petOwner: false,
+    employed: true,
   });
 
   const onInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
+    const { name, value, type } = e.target;
+    const checked = (e.target as HTMLInputElement).checked;
+    const val = type === "checkbox" ? checked : value;
+    setFormValues({ ...formValues, [name]: val });
   };
+
   const selectValues = ["canada", "usa"];
 
   return (
@@ -43,6 +49,25 @@ const Demo: React.FunctionComponent = () => {
         onChange={onInputChange}
         options={selectValues}
         placeholder={"Select A Country"}
+        required={true}
+      />
+      <h2>Example of a Checkbox Input</h2>
+      <CheckboxInput
+        name={"petOwner"}
+        label={
+          <>
+            Are You A Pet <span className="bold">Owner?</span>
+          </>
+        }
+        checked={formValues.petOwner}
+        onChange={onInputChange}
+        disabled={true}
+      />
+      <CheckboxInput
+        name={"employed"}
+        label={"Are You Currently Employed?"}
+        checked={formValues.employed}
+        onChange={onInputChange}
       />
     </div>
   );
