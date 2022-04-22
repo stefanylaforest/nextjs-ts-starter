8d5328ce6dc42fc1c88f661d1b6d1ec22a94df6a
Stefany Laforest <slaforest@spiria.com>
2022-04-22 11:57:11 -0400

    a few button test

--- src/ui-library/button/button.tsx
+++ src/ui-library/button/button.tsx
@@ -8,20 +8,22 @@ const Button = (props: ButtonProps): JSX.Element => {
     variant = "primary",
     children,
     onClick,
-    disabled = false,
+    disabled,
     type = "button",
     size = "large",
     ariaLabel,
   } = props;
   const stylesheet =
     variant === "icon" ? styles.icon : styles[`${variant}-${size}`];
+
+  const isDisabled = disabled ? { disabled: true } : {};
   return (
     <button
       className={stylesheet}
       onClick={onClick}
-      disabled={disabled}
       type={type}
       aria-label={ariaLabel}
+      {...isDisabled}
     >
       {children}
     </button>
