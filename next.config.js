ce4701850f2dc78b10eea1a185e81539e287cccb
Stefany Laforest <slaforest@spiria.com>
2022-04-28 09:27:08 -0400

    make children required in modal, single quotes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "src/styles")],
-    prependData: `@import "variables.scss";`,
+    includePaths: [path.join(__dirname, 'src/styles')],
+    prependData: '@import "variables.scss";',
   },
 };
--- src/ui-library/modal/interfaces.ts
+++ src/ui-library/modal/interfaces.ts
@@ -2,6 +2,6 @@ export default interface ModalProps {
   show: boolean;
   closeModal: () => void;
   title?: string;
-  children?: React.ReactNode;
+  children: React.ReactNode;
   footer?: React.ReactNode;
 }
