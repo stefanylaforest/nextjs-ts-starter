307e58ef5fd59d5237f01a229d168e6f176b9f30
Stefany Laforest <slaforest@spiria.com>
2022-05-05 11:34:34 -0400

    organize theme folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import type { NextPage } from 'next';
 
-import styles from '../styles/pages/index.module.scss';
-
 import Demo from '../components/demo/demo';
 
 const Home: NextPage = (): JSX.Element => {
   return (
-    <div className={styles.container}>
+    <div>
       <Demo />
     </div>
   );
