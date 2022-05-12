cb1bce7c56c6fd244390c303098f3bcd0bd23140
Stefany Laforest <slaforest@spiria.com>
2022-05-12 16:04:52 -0400

    rm comments

--- src/ui-library/toast/interfaces.tsx
+++ src/ui-library/toast/interfaces.tsx
@@ -1,4 +1,3 @@
-// Toast Provider
 export interface BasicToast {
   message: string | React.ReactNode;
   type: 'info' | 'success' | 'error' | 'warning';
@@ -12,7 +11,6 @@ export interface ToastContextType {
   activate: ({ message, type }: BasicToast) => void;
 }
 
-// Toast Component
 export default interface ToastProps extends ToastWithId {
   remove: (key: string) => void;
 }
