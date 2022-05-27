0bef3ab48c3158c67a5aa260cf76a051a1ed825b
Stefany Laforest <slaforest@spiria.com>
2022-05-27 09:57:22 -0400

    Versioning untracked files

--- src/ui-library/checkboxInput/interfaces.ts
+++
@@ -1,7 +0,0 @@
-export default interface CheckboxInputProps {
-  name: string;
-  label: string | React.ReactNode;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  checked: boolean;
-  disabled?: boolean;
-}
