703fabad9c85bc670154e791134d11c6c6706ad4
Stefany Laforest <slaforest@spiria.com>
2022-06-23 14:02:04 -0400

    rm svgr

--- next.config.js
+++ next.config.js
@@ -6,14 +6,6 @@ module.exports = {
     includePaths: [path.join(__dirname, 'src/theme')],
     prependData: '@import "variables.scss";',
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack']
-    });
-
-    return config;
-  }
 };
 
 
